dc102eb16003826bee03e5d7e417cd15c6146e73
Juan <juandiegovelsol@gmail.com>
2024-06-08 13:58:34 -0500

    fix: enpoint working great!

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import OpenAI from 'openai';
+import {
+  ChatCompletionTool,
+  ChatCompletionMessageParam,
+} from 'openai/resources';
 import axios from 'axios';
 import * as fs from 'fs';
 import * as csv from 'csv-parser';
@@ -21,16 +25,17 @@ interface Product {
 export class AppService {
   private openai: OpenAI;
   private products: Product[] = [];
+  private messages: ChatCompletionMessageParam[] = [];
+  private tools: ChatCompletionTool[] = [];
 
   constructor(private configService: ConfigService) {
-    console.log('Initializing AppService...');
     this.openai = new OpenAI({
       organization: this.configService.get<string>('OPENAI_ORG_ID'),
       apiKey: this.configService.get<string>('OPENAI_API_KEY'),
     });
     this.loadProducts();
   }
-  //FUNCIONA
+
   private loadProducts() {
     const products: Product[] = [];
     fs.createReadStream('./products_list.csv')
@@ -42,9 +47,8 @@ export class AppService {
         this.products = products;
       });
   }
-  //FUNCIONA
+
   private async searchProducts(query: string): Promise<Product[]> {
-    console.log('Searching products with query:', query);
     const relatedProducts = this.products.filter(
       (product) =>
         product.displayTitle.toLowerCase().includes(query.toLowerCase()) ||
@@ -52,13 +56,12 @@ export class AppService {
     );
     return relatedProducts.slice(0, 2);
   }
-  //FUNCIONA
+
   private async convertCurrency(
     amount: number,
     from: string,
     to: string,
   ): Promise<number> {
-    console.log(`Converting currency: ${amount} from ${from} to ${to}`);
     const apiKey = this.configService.get<string>('OPENEXCHANGERATES_API_KEY');
     const response = await axios.get(
       `https://openexchangerates.org/api/latest.json?app_id=${apiKey}`,
@@ -69,79 +72,119 @@ export class AppService {
   }
 
   public async handleChat(query: string): Promise<string> {
-    console.log('Handling chat query:', query);
+    this.messages = [
+      {
+        role: 'system',
+        content: 'You are a helpful assitant',
+      },
+      {
+        role: 'user',
+        content: `${query}, when asked for products select one article in a single word to shop from the store you believe fits my requirements, when asked for converting currencies do not suggest a product`,
+      },
+    ];
+    this.tools = [
+      {
+        type: 'function',
+        function: {
+          name: 'searchProducts',
+          description: 'Search for products',
+          parameters: {
+            type: 'object',
+            properties: {
+              query: { type: 'string' },
+            },
+            required: ['query'],
+          },
+        },
+      },
+      {
+        type: 'function',
+        function: {
+          name: 'convertCurrencies',
+          description: 'Convert currency',
+          parameters: {
+            type: 'object',
+            properties: {
+              amount: { type: 'number' },
+              from: { type: 'string' },
+              to: { type: 'string' },
+            },
+            required: ['amount', 'from', 'to'],
+          },
+        },
+      },
+    ];
     try {
       const response = await this.openai.chat.completions.create({
-        model: 'gpt-3.5-turbo-0613',
-        messages: [{ role: 'user', content: query }],
-        tools: [
-          {
-            type: 'function',
-            function: {
-              name: 'searchProducts',
-              description: 'Search for products',
-              parameters: {
-                type: 'object',
-                properties: {
-                  query: { type: 'string' },
-                },
-                required: ['query'],
-              },
-            },
-          },
-          {
-            type: 'function',
-            function: {
-              name: 'convertCurrencies',
-              description: 'Convert currency',
-              parameters: {
-                type: 'object',
-                properties: {
-                  amount: { type: 'number' },
-                  from: { type: 'string' },
-                  to: { type: 'string' },
-                },
-                required: ['amount', 'from', 'to'],
-              },
-            },
-          },
-        ],
-        tool_choice: 'auto',
+        model: 'gpt-3.5-turbo',
+        messages: this.messages,
+        tools: this.tools,
+        tool_choice: 'required',
       });
       const toolCall = response.choices[0]?.message?.tool_calls?.[0];
       const functionName = toolCall?.function.name;
-      const args = JSON.parse(toolCall.function.arguments || '{}');
-      console.log('Function arguments:', args);
+      const args = JSON.parse(toolCall?.function.arguments || '{}');
+      this.messages.push({
+        role: response.choices[0].message.role,
+        content: `Tool excecuted was: ${functionName}, found arguments: ${toolCall?.function.arguments}`,
+      });
       if (functionName === 'searchProducts') {
         const products = await this.searchProducts(args.query);
-        console.log('Products found:', products);
-        //Make other request for the final message;
-        const response = await this.openai.chat.completions.create({
-          model: 'gpt-3.5-turbo-0613',
-          messages: [
-            { role: 'user', content: query },
-            {
-              role: 'user',
-              content: `Generate an apropiate answer for user query: ${query}, with the following product information: ${JSON.stringify(products)}`,
-            },
-          ],
+        this.messages.push({
+          role: 'system',
+          content: `You excecuted tool ${functionName} and found product information: ${JSON.stringify(products)}. You must choose between generating an apropiate final answer (without mentioning anything about currencies) and to excecute the convertCurrencies tool. Select between this two options acording to the user needs expressed in query: ${query}. If you are not asked to convert the price to another currency DON'T DO IT`,
         });
-        return response.choices[0].message.content;
+        const response = await this.openai.chat.completions.create({
+          model: 'gpt-3.5-turbo',
+          messages: this.messages,
+          tools: this.tools,
+          tool_choice: 'auto',
+        });
+
+        const functionNameConvert =
+          response.choices[0]?.message?.tool_calls?.[0].function.name;
+        const argsConvert = JSON.parse(
+          response.choices[0]?.message?.tool_calls?.[0].function.arguments ||
+            '{}',
+        );
+
+        if (functionNameConvert === 'convertCurrencies') {
+          const convertedAmount = await this.convertCurrency(
+            argsConvert.amount,
+            argsConvert.from,
+            argsConvert.to,
+          );
+          this.messages.push(
+            {
+              role: response.choices[0].message.role,
+              content: `Tool excecuted was: ${functionNameConvert}, found arguments: ${JSON.stringify(argsConvert)} `,
+            },
+            {
+              role: 'system',
+              content: `Generate an apropiate answer for user query: ${query}, with the following product information: ${JSON.stringify(products)} and with the following currency information: Product price ${argsConvert.amount}, converted from ${argsConvert.from} to ${argsConvert.to}, converted price ${convertedAmount}`,
+            },
+          );
+          const finalResponse = await this.openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: this.messages,
+          });
+          return finalResponse.choices[0].message.content;
+        } else {
+          return response.choices[0].message.content;
+        }
       } else if (functionName === 'convertCurrencies') {
         const convertedAmount = await this.convertCurrency(
           args.amount,
           args.from,
           args.to,
         );
+        this.messages.push({
+          role: 'user',
+          content: `Generate an apropiate answer for user query: ${query}, with the following currency information: Initial amount ${args.amount}, converted from ${args.from} to ${args.to}, converted amount ${convertedAmount}`,
+        });
         const response = await this.openai.chat.completions.create({
-          model: 'gpt-3.5-turbo-0613',
-          messages: [
-            { role: 'user', content: query },
-            {
-              role: 'user',
-              content: `Generate an apropiate answer for user query: ${query}, with the following currency information: Initial amount ${args.amount}, converted from ${args.from} to ${args.to}, converted amount ${convertedAmount}`,
-            },
-          ],
+          model: 'gpt-3.5-turbo',
+          messages: this.messages,
         });
         return response.choices[0].message.content;
       } else {
@@ -149,7 +192,6 @@ export class AppService {
         return response.choices[0].message.content;
       }
     } catch (error) {
-      console.log(error);
       return error;
     }
   }
